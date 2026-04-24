57110f95a6dc74406da99983a2f8b0eaa280c03d
hussam_266195 <hussam_266195@svuonline.org>
2026-04-24 11:53:09 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using super_CLASS;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GO go = new GO();
+           
+           go.kj.v.z.moon.mxm = 5;
+            
+            Console.WriteLine(go.kj.v.z.moon.mxm);
+            Console.ResetColor();
+        }
+    }
+}
